825cf108a92dcc6c08bf4358adfe5e21a6d1b85b
Oliver Tang <yifengt@uchicago.edu>
2022-05-20 19:00:12 -0500

    organize data folder

--- data/drawRxn.py
+++
@@ -1,9 +0,0 @@
-from rdkit.Chem import Draw, AllChem
-
-def drawRxn(rxn, fileName='reaction.png'):
-    rxn = AllChem.ReactionFromSmarts(rxn)
-    d2d = Draw.MolDraw2DCairo(800, 300)
-    d2d.DrawReaction(rxn)
-    d2d.GetDrawingText()
-    png = d2d.GetDrawingText()
-    open(fileName, 'wb+').write(png)
