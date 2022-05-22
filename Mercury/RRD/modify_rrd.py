bbd9a45cce04e4d273d300b7b9bcdbdd729a07dc
Mercury <mercury1874@gmail.com>
2022-05-22 11:30:09 +0800

    update

--- Mercury/RRD/modify_rrd.py
+++ Mercury/RRD/modify_rrd.py
@@ -16,9 +16,37 @@ from bond import get_bond_basic_attrs
 from pchem import get_polarizability, get_GasteigerCharge, get_MR
 from joblib import Parallel, delayed
 from tqdm import tqdm
+import warnings
 
+warnings.filterwarnings("ignore")
 import os
 os.environ["CUDA_VISIBLE_DEVICES"]=""
+
+
+import pandas as pd
+import numpy as np
+from rdkit import Chem
+
+def one_of_k_encoding_unk(df, allowable_set=[1, 5, 6, 7, 8, 9, 12, 14, 15, 16, 17, 29, 30, 34, 35, 50, 53, 'other']):
+    """Maps inputs not in the allowable set to the last element."""
+    x = df['icnum']
+    if x not in allowable_set:
+        x = allowable_set[-1]
+    return list(map(lambda s: int(x == s), allowable_set))
+
+
+def get_atom_icnum(smiles):
+    res = []
+    mol = Chem.MolFromSmiles(smiles)
+    mol = Chem.AddHs(mol)
+    for atom in mol.GetAtoms():
+        anum = atom.GetAtomicNum()
+        res.append([atom.GetIdx(), anum])
+    df_anum = pd.DataFrame(res, columns = ['AtomIdx', 'icnum'])
+    df_anum = df_anum.set_index('AtomIdx')
+    new_df = df_anum.apply(lambda x: one_of_k_encoding_unk(x), axis=1, result_type='expand')
+    
+    return new_df
 
 def get_rrd(smiles, with_bv = False):
 
@@ -84,10 +112,7 @@ atom_feature_config = {'maxv': {'PC': 0.6302622556686401,
                         'MR': 14.02,
                         'APol': 10.6,
                         'SHI': 3.2216382711835254,
-                        'TSEI': 2.8153517921163136,
-                        'OV':200.0,
-                        'PBV':100.0,
-                        'PSV':100.0},
+                        'TSEI': 2.8153517921163136},
                        'minv': {'PC': -0.4310302436351776,
                         'FN': -0.061693187803030014,
                         'FE': -0.04397563263773918,
@@ -96,10 +121,7 @@ atom_feature_config = {'maxv': {'PC': 0.6302622556686401,
                         'MR': 0.0,
                         'APol': 0.557,
                         'SHI': 0.16777466065013924,
-                        'TSEI': 0.0,
-                        'OV':0.0,
-                        'PBV':0.0,
-                        'PSV':0.0}
+                        'TSEI': 0.0}
                       }
 
 bond_feature_config = {'maxv':{'SB': 1.0,
@@ -132,6 +154,24 @@ bond_feature_config = {'maxv':{'SB': 1.0,
                      'BDFE': 19.995909}
                     }
 
+
+def add_atom_idx(smiles):
+    mol = Chem.MolFromSmiles(smiles)
+    mol = Chem.AddHs(mol)
+    begins = []
+    ends = []
+    for i in range(mol.GetNumBonds()):
+        bond = mol.GetBondWithIdx(i)
+        begins.append(bond.GetEndAtomIdx())
+        ends.append(bond.GetBeginAtomIdx())
+
+    df_idx = pd.DataFrame({'BeginAtomIdx':begins, 'EndAtomIdx': ends})
+    df_idx.index.name = 'BondIdx'
+
+    return df_idx
+
+
+
 class RRDCalculator:
     
     def __init__(self, scale=True, with_bv = False):
@@ -142,14 +182,7 @@ class RRDCalculator:
         '''
         smiles: smile string
         '''
-        try:
-            dfa, dfb = get_rrd(smiles, with_bv = self.with_bv)
-            
-        except:
-            print('error when calculating %s' % smiles)
-            dfa = pd.DataFrame([], columns  = atom_feature_config['maxv'].keys())
-            dfb = pd.DataFrame([], columns  = bond_feature_config['maxv'].keys())
-            
+        dfa, dfb = get_rrd(smiles, with_bv = self.with_bv)
         if self.scale:
             a_scale = pd.DataFrame(atom_feature_config)
             a_scale['gap'] = a_scale.maxv - a_scale.minv
@@ -163,11 +196,51 @@ class RRDCalculator:
             dfb_scaled.BDE = dfb_scaled.BDE.fillna(1)
             dfb_scaled.BDFE = dfb_scaled.BDFE.fillna(1)
             dfb = dfb_scaled.fillna(0).clip(0,1)
+
+        df_icnum = get_atom_icnum(smiles)
+        dfa = dfa.join(df_icnum)
+        df_idx = add_atom_idx(smiles)
+        dfb = dfb.join(df_idx)
            
         return dfa, dfb
+
+
+
+        '''
+        try:
+            dfa, dfb = get_rrd(smiles, with_bv = self.with_bv)
+            if self.scale:
+                a_scale = pd.DataFrame(atom_feature_config)
+                a_scale['gap'] = a_scale.maxv - a_scale.minv
+                dfa_scaled = (dfa - a_scale.minv) / a_scale.gap
+                dfa_scaled = dfa_scaled[dfa.columns]
+                dfa = dfa_scaled.fillna(0).clip(0,1)
+
+                b_scale = pd.DataFrame(bond_feature_config)
+                b_scale['gap'] = b_scale.maxv - b_scale.minv
+                dfb_scaled = (dfb[b_scale.index] - b_scale.minv) / b_scale.gap
+                dfb_scaled.BDE = dfb_scaled.BDE.fillna(1)
+                dfb_scaled.BDFE = dfb_scaled.BDFE.fillna(1)
+                dfb = dfb_scaled.fillna(0).clip(0,1)
+            
+        except:
+            print('error when calculating %s' % smiles)
+            dfa = pd.DataFrame(data=[[0 for _ in range(len(atom_feature_config['maxv'].keys()))]], columns  = atom_feature_config['maxv'].keys(), index=[0])
+            dfb = pd.DataFrame(data=[[0 for _ in range(len(bond_feature_config['maxv'].keys()))]], columns  = bond_feature_config['maxv'].keys(), index=[0])
+            dfa.index.name = 'AtomIdx'
+            dfb.index.name = 'BondIdx'
+            
+        
+        df_icnum = get_atom_icnum(smiles)
+        dfa = dfa.join(df_icnum)
+        df_idx = add_atom_idx(smiles)
+        dfb = dfb.join(df_idx)
+           
+        return dfa, dfb
+    '''
     
     
     def batch_transform(self, smiles_list, n_jobs = -1):
         P = Parallel(n_jobs=n_jobs)
-        res = P(delayed(self.transform)(smiles,) for smiles in tqdm(smiles_list, ascii=True))
+        res = P(delayed(self.transform)(smiles,) for smiles in smiles_list)
         return res
